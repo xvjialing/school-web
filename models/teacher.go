47af066393d1adcb7d7b06062bbdbef64c5e97d8
xvjialing <xvjialing@outlook.com>
2020-08-30 14:18:56 +0800

    教师接口分页

--- models/teacher.go
+++ models/teacher.go
@@ -3,10 +3,11 @@ package models
 import (
 	"errors"
 	"fmt"
-	"reflect"
-	"strings"
-
 	"github.com/astaxie/beego/orm"
+	"math"
+	"reflect"
+	. "school-web/common"
+	"strings"
 )
 
 type Teacher struct {
@@ -119,6 +120,97 @@ func GetAllTeacher(query map[string]string, fields []string, sortby []string, or
 			}
 		}
 		return ml, nil
+	}
+	return nil, err
+}
+
+// GetAllTeacher retrieves all Teacher matches certain condition. Returns empty list if
+// no records exist
+func GetAllTeacherPage(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (techerPage *Page, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(Teacher))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	page := new(Page)
+	var ml []interface{}
+	var l []Teacher
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+
+		count, err := qs.Count()
+		if err != nil {
+			return nil, errors.New("Error: count error")
+		}
+		page.Total = count
+		page.Pages = int(math.Ceil((float64(count) / float64(limit))))
+		page.PageSize = limit
+		page.PageNum = offset
+
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		page.List = ml
+		return page, nil
 	}
 	return nil, err
 }
